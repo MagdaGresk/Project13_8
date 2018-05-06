b9d61808040bd7517ab63c99f34fa4119390920d
Magdalena Gresk <magdagresk@onet.pl>
2018-05-05 22:35:50 -0400

    Modify package.json and index.js

--- index.js
+++ index.js
@@ -4,17 +4,19 @@ var fs = require('fs')
 
 server.on('request', function (request, response) {
 	if (request.method === 'GET' && request.url === '/hello') {
-		fs.readFile('index.html', function(err, data) {
+		fs.readFile('./index.html', function(err, data) {
 		    response.writeHead(200, {'Content-Type': 'text/html'})
 		    response.write(data)
 		    response.end()
 		})
 	} else {
-		fs.readFile('cat.jpg', function(err, data)) {
-			response.statusCode(404, {'Content-Type': 'image/jpg'})
+		fs.readFile('./cat.jpg', function(err, data) {
+			response.writeHead(404, {'Content-Type': 'image/jpg'})
 			response.write(data)
 		    response.end()
-		}
+		})
 	}
 })
+
 server.listen(9000)
+console.log('Listening started')
