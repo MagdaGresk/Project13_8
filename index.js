a33c4840a32c320c651f93068761f31a97cd1367
Magdalena Gresk <magdagresk@onet.pl>
2018-05-05 21:02:32 -0400

    Create and add html, js and jpg files

---
+++ index.js
@@ -0,0 +1,20 @@
+var http = require('http')
+var server = http.createServer()
+var fs = require('fs')
+
+server.on('request', function (request, response) {
+	if (request.method === 'GET' && request.url === '/hello') {
+		fs.readFile('index.html', function(err, data) {
+		    response.writeHead(200, {'Content-Type': 'text/html'})
+		    response.write(data)
+		    response.end()
+		})
+	} else {
+		fs.readFile('cat.jpg', function(err, data)) {
+			response.statusCode(404, {'Content-Type': 'image/jpg'})
+			response.write(data)
+		    response.end()
+		}
+	}
+})
+server.listen(9000)
